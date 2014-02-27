56ec7bec1f8aaa44463c9aa892ae46b86ec9986c
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-02-27 11:42:03 +0000

    core idea/mechanics of manager class working ... next stop refactor

--- src/Manager.php
+++ src/Manager.php
@@ -14,17 +14,28 @@ class Manager {
 	protected $finder;
 
 	/** 
-	 * @var array of ModelInterface 
+	 * @var array of ModelInterface in the form of 'model.name' => $instance
 	 * @todo make this a proper typed array class
 	 */
 	protected $models = array();
 
-	/**
-	 * @todo explain
+	/** 
+	 * @var array of ModelStorageHandlerInterface in the form of 'storage.handler.name' => $instance
+	 * @todo make this a proper typed array class
 	 */
-	public function setDefaultStorageHandler(ModelStorageHandlerInterface $handler) {
-		$this->defaultStorageHandler = $handler;
-	}
+	protected $modelStorageHandlers = array();
+
+	/** 
+	 * @var array of key pair stings 'model.name' => 'storage.handler.name'
+	 * @todo make this a proper typed array class
+	 */
+	protected $modelToStorageHandlerMap = array();
+
+	/** @var array of key pair stings 'model.name' => 'Full\Namespaced\Model\Class' */
+	protected $modelClassList;
+
+	/** @var int used to create unique name for every registered anonymous storage handler */
+	protected $anonymousHandlerCount = 0;
 
 	/**
 	 * @todo explain
@@ -34,29 +45,93 @@ class Manager {
 	}
 
 	/**
-	 * @todo explain
+	 * @throws Exception when the requested model name does not exist
+	 * @todo explain and better exception
 	 */
 	public function getModelClass($modelName) {
-		$classlist = $this->finder->getModelClassList();
-		return $classlist[$modelName];
+
+		if(!$this->modelClassList) {
+			$this->modelClassList = $this->finder->getModelClassList();
+		}
+
+		if(!isset($this->modelClassList[$modelName])) {
+			throw new \Exception(sprintf('No model found registered under the name %s', $modelName));
+		}
+
+		return $this->modelClassList[$modelName];
+	}
+
+	public function loadModel($class) {
+		return new $class;
 	}
 
 	/**
+	 * get a preconfiguered model 
+	 * Note: setStorageHandler is called on the Model every time it is requested. Seems like a good idea to allow
+	 * for storage handler to be updated during runtime.
 	 * @todo explain
 	 */
 	public function get($modelName) {
 
 		if(!isset($this->models[$modelName])) {
 			$class = $this->getModelClass($modelName);
-
-			$model = new $class();
-			$model->setStorageHandler($this->defaultStorageHandler);
-
-			$this->models[$modelName] = $model;
-		} else {
-			$model = $this->models[$modelName];
+			$this->models[$modelName] = $this->loadModel($class);
 		}
+
+		$model = $this->models[$modelName];
+		$model->setStorageHandler($this->getModelsStorageHandler($modelName));
 
 		return $model;
 	}
+
+	/**
+	 * @todo explain
+	 */
+	public function setModelStorageHandler(ModelStorageHandlerInterface $handler, $storageName = null) {
+		if(!$storageName) {
+			if(!$this->defaultStorageHandler) {
+				$this->setDefaultStorageHandler($handler);
+			} else {
+				$this->anonymousHandlerCount += 1;
+				$this->setModelStorageHandler($handler, 'storage.handler.' . $this->anonymousHandlerCount);
+			}
+		} else {
+			$this->modelStorageHandlers[$storageName] = $handler;
+		}
+	}
+
+	/**
+	 * @todo explain
+	 */
+	public function setDefaultStorageHandler(ModelStorageHandlerInterface $handler) {
+		$this->defaultStorageHandler = $handler;
+		$this->setModelStorageHandler($handler, 'default');
+	}
+
+	/**
+	 * @todo explain
+	 */
+	public function setModelToStorageHandlerMap(array $map) {
+		$this->modelToStorageHandlerMap = $map;
+	}
+
+	/**
+	 * @throws Exception when handler cannot be found
+	 * @todo explain
+	 */
+	public function getModelsStorageHandler($modelName) {
+		$handler = $this->defaultStorageHandler;
+		if(isset($this->modelToStorageHandlerMap[$modelName])) {
+			$handlerName = $this->modelToStorageHandlerMap[$modelName];
+			$handler = $this->modelStorageHandlers[$handlerName];
+		}
+
+		if(!$handler) {
+			throw new \Exception(sprintf("No storage handler can be found for the model '%s'", $modelName));
+		}
+
+		return $handler;
+	}
+
+
 }
--- src/Test/ManagerTest.php
+++ src/Test/ManagerTest.php
@@ -3,6 +3,7 @@
 namespace Skip\Model\Test;
 
 use Skip\Model\Manager;
+use Skip\Model\ModelStorageHandlerInterface;
 
 class ManagerTest extends \PHPUnit_Framework_TestCase {
 
@@ -30,5 +31,62 @@ class ManagerTest extends \PHPUnit_Framework_TestCase {
 		$modelB = $manager->get('model.class.a');
 
 		$this->assertSame($modelA, $modelB);
+	}
+
+	/**
+	 * test get method model with multiple storage handlers
+	 */
+	public function testGetModelsWithMultipleStorageHandlers() {
+
+		$mockStorageHandlerDefault = $this->getMock('Skip\Model\ModelStorageHandlerInterface', array(), array(), '', FALSE);
+		$mockStorageHandlerCustom = $this->getMock('Skip\Model\ModelStorageHandlerInterface', array(), array(), '', FALSE);
+
+		$mockModelA = $this->getMock('Skip\Model\ModelInterface', array('setStorageHandler'), array(), '', FALSE);
+		$mockModelA->expects($this->once())
+			->method('setStorageHandler')
+			->will($this->returnCallback(function(ModelStorageHandlerInterface $handler) use ($mockStorageHandlerDefault) {
+				$this->assertSame($mockStorageHandlerDefault, $handler);
+			}));
+
+		$mockModelB = $this->getMock('Skip\Model\ModelInterface', array('setStorageHandler'), array(), '', FALSE);
+		$mockModelB->expects($this->once())
+			->method('setStorageHandler')
+			->will($this->returnCallback(function(ModelStorageHandlerInterface $handler) use ($mockStorageHandlerCustom) {
+				$this->assertSame($mockStorageHandlerCustom, $handler);
+			}));
+
+		$mockModelClassA = get_class($mockModelA);
+		$mockModelClassB = get_class($mockModelB);
+
+		$this->assertNotSame($mockModelA, $mockModelB);
+
+		$mockModels = array($mockModelA, $mockModelB);
+
+		$manager = $this->getMock('Skip\Model\Manager', array('loadModel'), array(), '', TRUE);
+		$manager->expects($this->any())
+			->method('loadModel')
+			->will($this->returnCallback(function($class) use (&$mockModels) {
+				$model = array_shift($mockModels);
+				return $model;
+			}));
+
+		$mockFinder = $this->getMock('Skip\Model\ModelFinderInterface', array('getModelClassList'), array(), '', FALSE);
+		$mockFinder->expects($this->once())
+			->method('getModelClassList')
+			->will($this->returnValue(array(
+				'model.class.a' => $mockModelClassA,
+				'model.class.b' => $mockModelClassB)));
+		$manager->setFinder($mockFinder);
+
+		$manager->setModelStorageHandler($mockStorageHandlerDefault);
+		$manager->setModelStorageHandler($mockStorageHandlerCustom, 'storage.handler.b');
+
+		$manager->setModelToStorageHandlerMap(array(
+			'model.class.b' => 'storage.handler.b'
+			));
+		$modelA = $manager->get('model.class.a');
+		$modelB = $manager->get('model.class.b');
+
+		$this->assertNotSame($modelA, $modelB);
 	}
 }

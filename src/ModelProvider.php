07780d8b0183af6549ec58b312910f4e18be3849
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-02-28 09:06:13 +0000

    add model provider test ... failing ;)

---
+++ src/ModelProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Skip\Model;
+
+use Silex\ServiceProviderInterface;
+use Silex\Application;
+
+/**
+ * @todo explain
+ */
+class ModelProvider implements ServiceProviderInterface {
+
+	public function register(Application $app) {
+
+	}
+
+	public function boot(Application $app) {
+
+	}
+}
---
+++ test/src/ModelProviderTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Skip\Model\Test;
+
+use Skip\Model\ModelProvider;
+use Silex\Application;
+
+class ModelProviderTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * test model manager service is available
+	 */
+	public function testModelManagerServiceIsAvailable() {
+		$app = new Application();
+		$app->register(new ModelProvider());
+
+		$modelManager = $app['model'];
+
+		$this->assertInstanceOf('Skip\Model\ModelManager', $modelManager);
+	}
+}

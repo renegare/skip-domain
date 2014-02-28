4011b06edd30fe5df7378f649dac8fcd7c85d713
Mudi Ugbowanko <mudi.ugbowanko@mendeley.com>
2014-02-28 16:10:34 +0000

    test service provider can actually find a given model

--- src/ModelProvider.php
+++ src/ModelProvider.php
@@ -12,8 +12,41 @@ class ModelProvider implements ServiceProviderInterface {
 
 	public function register(Application $app) {
 		$app['model'] = function(Application $app) {
-			return new Manager();
+			$manager = new Manager();
+			$manager->setFinder($app['model.finder']);
+
+			$storageHandlerServices = $app['model.storage.handlers'];
+
+			if(!is_array($storageHandlerServices)) {
+				throw new \Exception("Service 'model.storage.handlers' must contain an array of storage handlers");
+			}
+
+			if(!count($storageHandlerServices)) {
+				throw new StorageHandlerNotFoundException("No storage handler service(s) found. Please register atleast one.");
+			}
+
+			foreach($storageHandlerServices as $storageHandlerName => $storageHandlerService) {
+				if(is_string($storageHandlerService)) {
+					$storageHandlerService = $app[$storageHandlerService];
+				}
+				$manager->setModelStorageHandler($storageHandlerService, $storageHandlerName);
+			}
+
+			$storageMap = isset($app['model.storage.handler.map']) ? $app['model.storage.handler.map'] : array();
+			foreach($storageMap as $modelName => $handlerService) {
+				$manager->setModelHandlerOverride($modelName, $app[$handlerService]);
+			}
+
+			return $manager;
 		};
+
+		$app['model.finder'] = function(Application $app) {
+			$finder = new Finder\FileFinder();
+			$finder->setNamespace($app['model.finder.namespace']);
+			$finder->setBasePath($app['model.finder.path']);
+			return $finder;
+		};
+
 	}
 
 	public function boot(Application $app) {
--- test/src/ModelProviderTest.php
+++ test/src/ModelProviderTest.php
@@ -8,14 +8,59 @@ use Silex\Application;
 class ModelProviderTest extends \PHPUnit_Framework_TestCase {
 
 	/**
-	 * test model manager service is available
+	 * test model service is available
 	 */
 	public function testModelManagerServiceIsAvailable() {
 		$app = new Application();
-		$app->register(new ModelProvider());
+		$app->register(new ModelProvider(), array(
+				'model.finder.namespace' => 'Fake\NameSpace',
+				'model.finder.path' => '/tmp/fake'
+			));
+		
+		$mockStorage = $this->getMock('Skip\Model\ModelStorageHandlerInterface');
+		$app['model.storage.handlers'] = array('mock.storage' => $mockStorage);
 
 		$modelManager = $app['model'];
 
 		$this->assertInstanceOf('Skip\Model\Manager', $modelManager);
+	}
+
+	/**
+	 * test model.finder service is available
+	 */
+	public function testModelFinderServiceIsAvailable() {
+		$app = new Application();
+		$app->register(new ModelProvider(), array(
+				'model.finder.namespace' => 'Fake\NameSpace',
+				'model.finder.path' => '/tmp/fake'
+			));
+
+		$finder = $app['model.finder'];
+
+		$this->assertInstanceOf('Skip\Model\ModelFinderInterface', $finder);
+	}
+
+	/**
+	 * test model service can find a model!
+	 */
+	public function testModelServiceCanFindAModel() {
+		$app = new Application();
+		$app->register(new ModelProvider(), array(
+				'model.finder.namespace' => 'Fake\NameSpace',
+				'model.finder.path' => '/tmp/fake'
+			));
+		
+		$mockModel = $this->getMock('Skip\Model\ModelInterface', array('setStorageHandler'), array(), '', FALSE);
+		$mockModelClass = get_class($mockModel);
+		$mockFinder = $this->getMock('Skip\Model\ModelFinderInterface', array('getModelClassList'), array(), '', FALSE);
+		$mockFinder->expects($this->once())
+			->method('getModelClassList')
+			->will($this->returnValue(array('a.model' => $mockModelClass)));
+		$app['model.finder'] = $mockFinder;
+
+		$mockStorage = $this->getMock('Skip\Model\ModelStorageHandlerInterface');
+		$app['model.storage.handlers'] = array('mock.storage' => $mockStorage);
+
+		$this->assertInstanceOf('Skip\Model\ModelInterface', $app['model']->get('a.model'));
 	}
 }
